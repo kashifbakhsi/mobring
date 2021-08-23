d4c16dfbc57836a731c7f4701c2992d0b7bb9427
kashifbakhsi <89003601+kashifbakhsi@users.noreply.github.com>
2021-08-23 15:46:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function search_ring() {
+  let input = document.getElementById('searchBar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('box');
+
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+          
+      }
+      else {
+          x[i].style.display="list-item";  
+         
+      }
+  }
+}
+
